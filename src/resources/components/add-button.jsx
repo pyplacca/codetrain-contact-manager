65f558658fce93db08b37517b7d7cb8693e884ae
pyPlacca <dhegreatprosperity10@gmail.com>
2020-08-05 15:24:16 +0000

    Create add button component

---
+++ src/resources/components/add-button.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Icons from '../icons.jsx'
+
+
+class AddButton extends React.Component {
+	render () {
+		return (
+			<button
+				className="add"
+				onClick={this.props.clickCallback}
+			>
+				<Icons.Add />
+			</button>
+		)
+	}
+}
+
+
+export default AddButton
