6b9e6680aa7e921893f7566984faec29737a048f
pyPlacca <dhegreatprosperity10@gmail.com>
2020-08-08 13:20:43 +0000

    Update group-card.jsx
    Adds id to props, renames "person(s)" to "contact(s)".
    
    clickCallback is used to preview a contact's details when the contact's card is clicked on

--- src/components/cards/group-card.jsx
+++ src/components/cards/group-card.jsx
@@ -4,13 +4,13 @@ import React from 'react'
 class GroupCard extends React.Component {
 
 	render () {
-		const {name, list} = this.props
+		const {name, list, clickCallback} = this.props
 		const total = list.length
 		return (
-			<section className="card group simple-view">
+			<section id={name} className="card group" onClick={clickCallback}>
 				<p className="name">{name}</p>
 				<p className="count">
-					{`${total} person${total > 1 ? 's' : ''}`}
+					{`${total} contact${total > 1 ? 's' : ''}`}
 				</p>
 			</section>
 		)
