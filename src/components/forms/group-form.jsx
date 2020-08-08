f69bda68a30646396a0582c1f5100f3cc577c680
pyPlacca <dhegreatprosperity10@gmail.com>
2020-08-08 13:26:14 +0000

    Makes group form multipurpose.
    The group form can now be used to edit/modify existing groups.

--- src/components/forms/group-form.jsx
+++ src/components/forms/group-form.jsx
@@ -1,36 +1,67 @@
-import React from 'react'
-import form from './form.jsx'
+import React from "react"
+import form from "./form.jsx"
 
 
 class GroupForm extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			name: undefined
+		}
+
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	handleChange ({target}) {
+		console.log(target.value, target.checked)
+	}
+
 	render () {
-		const {contact_list} = this.props
+		const {contacts, preview} = this.props
 		return (
 			<form.Form
-				title="New Group"
+				title={!preview ? "New Group" : "Update Group"}
 				id="group-form"
 			>
 				<form.FormField label="Group Name">
-					<input type="text" placeholder="Enter group name"/>
+					<input
+						type="text"
+						placeholder="Enter group name"
+						value={preview}
+						onChange={this.handleChange}
+					/>
 				</form.FormField>
 
 				<form.FormField className="field" label="Select Contacts" />
 				<div className="checklist">
 					{
-						contact_list.map((contact, i) =>
+						Object.values(contacts).map((contact, i) =>
 							<form.FormField key={i}>
-								<input type="checkbox"/>
-								{contact.name}
+								<input
+									type="checkbox"
+									checked={preview && contact.group.includes(preview)}
+									onChange={this.handleChange}
+									value={contact.id}
+								/>
+								{contact.name || contact.number}
 							</form.FormField>
 						)
 					}
 				</div>
 				<form.FormField>
-					<input type="submit" value="Create Group"/>
+					<input
+						type="submit"
+						value={(preview ? 'Update' : 'Create Group')}
+					/>
 				</form.FormField>
 			</form.Form>
 		)
 	}
+}
+
+GroupForm.defaultProps = {
+	contacts: {},
 }
 
 
