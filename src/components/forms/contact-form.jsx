adf553aea284663aea5f5f65e68fdf297c48cac0
pyPlacca <dhegreatprosperity10@gmail.com>
2020-08-08 13:27:03 +0000

    Makes contact form multipurpose
    The contact form can now be used to modify a contact's details.

--- src/components/forms/contact-form.jsx
+++ src/components/forms/contact-form.jsx
@@ -1,42 +1,74 @@
-import React from 'react'
-import form from './form.jsx'
+import React from "react"
+import form from "./form.jsx"
 
 
 class ContactForm extends React.Component {
 
+	modes = {
+		preview: "",
+		edit: "Edit Contact",
+		add: "New Contact"
+	}
+
 	render () {
+		// props: mode [preview, edit], contact (contact to be previewed or edited)
+		let {mode, contact} = this.props
+		if (contact.name && mode !== "edit") {
+			mode = "preview"
+		}
+		const disabled = mode === "preview"
+
 		return (
 			<form.Form
-				title="New Contact"
+				title={this.modes[mode]}
 				id="contact-form"
+				className={` ${mode}-mode`}
 			>
 				<form.FormField label="Name">
 					<input
 						type="text"
 						placeholder="Enter contact name"
+						disabled={disabled}
+						name="name"
+						value={contact.name}
+
 					/>
 				</form.FormField>
 				<form.FormField label="Mobile Number">
 					<input
 						type="tel"
 						placeholder="Enter mobile number"
+						disabled={disabled}
+						name="number"
+						value={contact.number}
 					/>
 				</form.FormField>
 				<form.FormField label="Email Address">
 					<input
 						type="email"
 						placeholder="Email address"
+						disabled={disabled}
+						name="email"
+						value={contact.email}
 					/>
 				</form.FormField>
-				<form.FormField>
-					<input
-						type="submit"
-						value="Add Contact"
-					/>
-				</form.FormField>
+				{
+					mode === "preview" ? null :
+					<form.FormField>
+						<input
+							type="submit"
+							value={mode === "edit" ? "Update" : "Add Contact"}
+						/>
+					</form.FormField>
+				}
 			</form.Form>
 		)
 	}
+}
+
+ContactForm.defaultProps = {
+	mode: "add",
+	contact: {},
 }
 
 
