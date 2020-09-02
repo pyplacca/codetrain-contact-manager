490ba6f5a2447b758bccf35af00041234d4b0281
pyPlacca <dhegreatprosperity10@gmail.com>
2020-09-02 18:20:54 +0000

    create and initialize redux store

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import * as serviceWorker from './serviceWorker';
+import reducer from './store/reducer'
+
+
+const store = createStore(reducer)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
