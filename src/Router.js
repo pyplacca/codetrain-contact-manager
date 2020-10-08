3612ea964de645f1baf14aac5025a7cf15ca8473
pyPlacca <dhegreatprosperity10@gmail.com>
2020-10-08 17:35:08 +0000

    Create Router.js

---
+++ src/Router.js
@@ -0,0 +1,16 @@
+// import React from "react";
+// import { BrowserRouter, Route } from 'react-router-dom';
+// import { App, Login, Misc } from './components';
+
+
+export default function Router () {
+	return null
+	/*
+	return (
+		<BrowserRouter>
+			<Misc.ProtectedRoute exact path="/" component={App} />
+			<Route exact path="/login" component={Login} />
+		</BrowserRouter>
+	)
+	*/
+}
