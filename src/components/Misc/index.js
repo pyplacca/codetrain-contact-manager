35abbbb6f28e25494721b79e4cde24db37dd48be
pyPlacca <dhegreatprosperity10@gmail.com>
2020-10-10 10:53:13 +0000

    add created components (Loading, Header, Footer) to Misc module

--- src/components/Misc/index.js
+++ src/components/Misc/index.js
@@ -1,6 +1,9 @@
 import ContentCategory from './ContentCategory';
 import ContentDisplay from './ContentDisplay';
 import ProtectedRoute from './ProtectedRoute';
+import Loading from './Loading';
+import Header from './Header';
+import Footer from './Footer';
 
 import * as Buttons from './Buttons';
 import * as Icons from './Icons';
@@ -11,4 +14,7 @@ export {
 	ContentCategory,
 	ContentDisplay,
 	ProtectedRoute,
+	Loading,
+	Header,
+	Footer,
 };
