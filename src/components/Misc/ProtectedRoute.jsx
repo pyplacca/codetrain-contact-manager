4825b471a5fd26306dd963c7d5f1c0a18e9480ac
pyPlacca <dhegreatprosperity10@gmail.com>
2020-10-10 22:02:02 +0000

    fix redirect issues

--- src/components/Misc/ProtectedRoute.jsx
+++ src/components/Misc/ProtectedRoute.jsx
@@ -1,40 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
-// import { Log}
-import { Pages, Misc } from 'components'
+import { Misc } from 'components'
 
 class ProtectedRoute extends React.Component {
 	render () {
-		const {component:Page, frbauth, ...other} = this.props;
-		console.log(frbauth)
-		const {isLoaded, uid} = frbauth;
-		// return <Redirect to={{pathname: 'login'}} />
-		// return <Misc.Loading />
+		const {component:Page, fbauth, ...other} = this.props;
+		const {isLoaded, uid} = fbauth;
+
 		if (!isLoaded) {
 			return <Misc.Loading />
 		}
-		if (isLoaded && uid) {
-			return (
-				<Route
+		if (isLoaded) {
+			if (uid) {
+				return <Route
 					{...other}
 					render={
 						props => <Page {...props} />
 					}
 				/>
-			);
+			}
+			return <Redirect to={{pathname: '/login'}} />
 		}
-		return <Route {...other} component={Pages.Login} />
 	};
 };
 
 const mapStateToProps = state => ({
-	frbauth: state.firebaseReducer.auth,
+	fbauth: state.firebaseReducer.auth,
 });
 
-const mapDispatchToProps = {
-
-};
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(ProtectedRoute);
+export default connect(mapStateToProps, null)(ProtectedRoute);
