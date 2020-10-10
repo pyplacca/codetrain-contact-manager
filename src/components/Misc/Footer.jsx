f7af84d67c56ac2049422cb4c3687f23178c3e79
pyPlacca <dhegreatprosperity10@gmail.com>
2020-10-10 22:00:47 +0000

    use app version .env variable

--- src/components/Misc/Footer.jsx
+++ src/components/Misc/Footer.jsx
@@ -5,10 +5,15 @@ import "static/css/footer.css";
 export default function Footer () {
 	return (
 		<div className="Footer">
-			<p>Contact Manager v4.0.1</p>
+			<p>
+				{
+					'Contact Manager v' +
+					process.env.REACT_APP_VERSION
+				}
+			</p>
 			<div className="Footer__signature">
 				<p>&copy; 2020 - David Placca - Codetrain Gen 14</p>
 			</div>
 		</div>
-	)
-}
+	);
+};
