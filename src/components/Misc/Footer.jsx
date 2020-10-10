c5394eef04f28453d387c514097cd2b0918a42fa
pyPlacca <dhegreatprosperity10@gmail.com>
2020-10-10 10:51:11 +0000

    Create Footer component

---
+++ src/components/Misc/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "static/css/footer.css";
+
+
+export default function Footer () {
+	return (
+		<div className="Footer">
+			<p>Contact Manager v4.0.1</p>
+			<div className="Footer__signature">
+				<p>&copy; 2020 - David Placca - Codetrain Gen 14</p>
+			</div>
+		</div>
+	)
+}
