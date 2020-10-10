c1ff80d18c6784fdcf03fca91badae04bd6dd021
pyPlacca <dhegreatprosperity10@gmail.com>
2020-10-10 21:48:40 +0000

    add sign in, sign up and sign out actions

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,29 +1,51 @@
-export function logIn (using) {
+export function signIn (using, credentials={}) {
 	return (dispatch, getState, {fb}) => {
-		// fb.auth()
-		// .then()
-		dispatch({
-			type: 'LOG_IN',
-			payload: {
-				user: null
-			}
-		})
-	}
-}
+		const firebase = fb()
+		if (using !== 'email') {
+			const provider = using === 'google' ?
+				firebase.auth.GoogleAuthProvider : (
+					using === 'github' ?
+					firebase.auth.GithubAuthProvider : (
+						using === 'apple' ?
+						firebase.auth.OAuthProvider : (
+							using === 'twitter' ?
+							firebase.auth.TwitterAuthProvider :
+							firebase.auth.FacebookAuthProvider
+						)
+					)
+				)
+			firebase.auth().signInWithRedirect(new provider());
+		} else {
+			const {email, password} = credentials;
+			firebase.auth().signInWithEmailAndPassword(email, password)
+			.then(() => dispatch({
+				type: 'SET_LOG_IN_ERROR',
+				payload: null
+			}))
+			.catch(err => dispatch({
+				type: 'SET_LOG_IN_ERROR',
+				payload: err
+			}))
+		};
+	};
+};
 
-export function signUp (using) {
+export function signUp (email, password) {
 	return (dispatch, getState, {fb}) => {
-		dispatch({
-			type: 'SIGN_UP',
-			payload: {
-
-			}
-		})
-	}
-}
+		fb().auth().createUserWithEmailAndPassword(email, password)
+		.then(() => dispatch({
+			type: 'SET_SIGN_UP_ERROR',
+			payload: null
+		}))
+		.catch(err => dispatch({
+			type: 'SET_SIGN_UP_ERROR',
+			payload: err
+		}));
+	};
+};
 
 export function signOut () {
 	return (dispatch, getState, {fb}) => {
-		return null
-	}
-}
+		fb().auth().signOut();
+	};
+};
