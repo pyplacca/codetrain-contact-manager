5e45885d9a447ffc532a9663addd97a6f0fdbefe
pyPlacca <dhegreatprosperity10@gmail.com>
2020-10-10 10:55:59 +0000

    Update config.js

--- src/store/config.js
+++ src/store/config.js
@@ -1,16 +1,17 @@
-import firebase from '../firebase/config';
+import firebase from 'firebase/config';
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { getFirebase, reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
 import { getFirestore, reduxFirestore } from 'redux-firestore';
-import mainReducer from './reducers/main'
+import { mainReducer, authReducer } from './reducers'
 
 
 // integrate firebase with redux store
 export default createStore(
 	combineReducers({
 		firebaseReducer,
-		mainReducer
+		mainReducer,
+		authReducer,
 	}),
 	// middleware
 	compose(
