71d136be743cc53c16ed99b6dcef99648eeea085
pyPlacca <dhegreatprosperity10@gmail.com>
2020-10-10 10:54:31 +0000

    Update ProtectedRoute.jsx

--- src/components/Misc/ProtectedRoute.jsx
+++ src/components/Misc/ProtectedRoute.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
+// import { Log}
+import { Pages, Misc } from 'components'
 
 class ProtectedRoute extends React.Component {
 	render () {
-		// const {component:Page, ...other} = this.props;
-		return null;
-		/*return (
-			<Route
-				{...other}
-				render={
-					props => <Page {...props} />
-				}
-			/>
-		);*/
+		const {component:Page, frbauth, ...other} = this.props;
+		console.log(frbauth)
+		const {isLoaded, uid} = frbauth;
+		// return <Redirect to={{pathname: 'login'}} />
+		// return <Misc.Loading />
+		if (!isLoaded) {
+			return <Misc.Loading />
+		}
+		if (isLoaded && uid) {
+			return (
+				<Route
+					{...other}
+					render={
+						props => <Page {...props} />
+					}
+				/>
+			);
+		}
+		return <Route {...other} component={Pages.Login} />
 	};
 };
 
 const mapStateToProps = state => ({
-	fbauth: state.firebaseReducer.auth,
+	frbauth: state.firebaseReducer.auth,
 });
 
 const mapDispatchToProps = {
