f1861d8b6b19760499fb79ab77d55e2497dbfe84
pyPlacca <dhegreatprosperity10@gmail.com>
2020-10-12 15:02:41 +0000

    fix display of two stacked Loading components on same screen during authentication

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
-import Loading from './Loading';
 // import { Misc } from 'components'
 
 class ProtectedRoute extends React.Component {
@@ -10,8 +9,7 @@ class ProtectedRoute extends React.Component {
 		const {isLoaded, uid} = fbauth;
 
 		if (!isLoaded) {
-			return <Loading />
-			// return <Misc.Loading />
+			return null
 		}
 		if (isLoaded) {
 			if (uid) {
